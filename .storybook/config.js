0a81c229c9644c173920379b9a464c45d529962c
yokikoto <endou.asako@mirait.co.jp>
2018-09-18 21:36:42 +0900

    todomvcのcssを追加

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import { configure } from '@storybook/riot';
+import '../node_modules/todomvc-app-css/index.css'
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
