ed47df48ea000983e041dece084f2f973186b114
yokikoto <endou.asako@mirait.co.jp>
2018-09-20 20:53:45 +0900

    add footer storys

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -41,4 +41,32 @@ storiesOf('Footer', module)
       onShow: action('onShow')
     })
   )
+  .add('show completed', () =>
+    tag('test', '<div class="todoapp"><app-footer completed-count="{ 10 }" active-count="{ 4 }" filter="Completed" on-clear-completed="{ onClearCompleted }" on-show="{ onShow }"></app-footer></div>', '', '', () => { }) &&
+    mount('test', {
+      onClearCompleted: action('onClearCompleted'),
+      onShow: action('onShow')
+    })
+  )
+  .add('show active', () =>
+    tag('test', '<div class="todoapp"><app-footer completed-count="{ 10 }" active-count="{ 4 }" filter="Active" on-clear-completed="{ onClearCompleted }" on-show="{ onShow }"></app-footer></div>', '', '', () => { }) &&
+    mount('test', {
+      onClearCompleted: action('onClearCompleted'),
+      onShow: action('onShow')
+    })
+  )
+  .add('no items', () =>
+    tag('test', '<div class="todoapp"><app-footer completed-count="{ 10 }" active-count="{ 0 }" filter="All" on-clear-completed="{ onClearCompleted }" on-show="{ onShow }"></app-footer></div>', '', '', () => { }) &&
+    mount('test', {
+      onClearCompleted: action('onClearCompleted'),
+      onShow: action('onShow')
+    })
+  )
+  .add('only one', () =>
+    tag('test', '<div class="todoapp"><app-footer completed-count="{ 10 }" active-count="{ 1 }" filter="All" on-clear-completed="{ onClearCompleted }" on-show="{ onShow }"></app-footer></div>', '', '', () => { }) &&
+    mount('test', {
+      onClearCompleted: action('onClearCompleted'),
+      onShow: action('onShow')
+    })
+  )
   ;
